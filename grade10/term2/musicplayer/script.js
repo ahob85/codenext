10ec834b88d6e1368f937dab6618945c07340d9d
ahobson <ahobson@google.com>
2017-12-21 20:56:15 -0500

    buttons

--- grade10/term2/musicplayer/script.js
+++ grade10/term2/musicplayer/script.js
@@ -1,8 +1,24 @@
 var button = document.getElementById("button");
 var breezewax = document.getElementById("breezewax");
-button.addEventListener("click", playSong);
+button.addEventListener("click", playButton);
 
-function playSong(){
-  alert("Hello!");
+function playSong() {
+  alert("Playing");
   breezewax.play();
+  button.innerHTML = "Pause!";
+}
+
+function pauseSong() {
+  alert("Pausing");
+  breezewax.pause();
+  button.innerHTML = "Play!";
+}
+
+function playButton() {
+  if(breezewax.paused) {
+    playSong();
+  }
+  else {
+    pauseSong();
+  }
 }
