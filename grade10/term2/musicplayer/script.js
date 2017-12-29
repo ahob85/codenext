56bc4f163fc3afb45f60c9edd934f1c874fc2c19
ahob85 <aaron@ahob85.com>
2017-12-29 13:43:02 -0800

    namespace!

--- grade10/term2/musicplayer/script.js
+++ grade10/term2/musicplayer/script.js
@@ -1,104 +1,104 @@
 (function() {
     
-var playButton = document.getElementById("play-button");
+    var playButton = document.getElementById("play-button");
 
-var volumeSlider = document.getElementById("volume-slider");
-var currentVolume = document.getElementById("current-volume");
+    var volumeSlider = document.getElementById("volume-slider");
+    var currentVolume = document.getElementById("current-volume");
 
-var trackButtons = document.getElementsByClassName("track-select");
-var trackTitles = document.getElementsByClassName("track-title");
-var tracks = document.getElementsByClassName("track");
+    var trackButtons = document.getElementsByClassName("track-select");
+    var trackTitles = document.getElementsByClassName("track-title");
+    var tracks = document.getElementsByClassName("track");
 
-var currentTrackID = 0;
-var currentTrack = tracks[currentTrackID];
-var currentTrackDisplay = document.getElementById("current-track-display");
+    var currentTrackID = 0;
+    var currentTrack = tracks[currentTrackID];
+    var currentTrackDisplay = document.getElementById("current-track-display");
 
-var artistImage = document.getElementById("artist-image");
+    var artistImage = document.getElementById("artist-image");
 
-var trackDurations = document.getElementsByClassName("track-duration");
+    var trackDurations = document.getElementsByClassName("track-duration");
 
-playButton.addEventListener("click", togglePlayPause);
-volumeSlider.addEventListener("input", changeVolume);
+    playButton.addEventListener("click", togglePlayPause);
+    volumeSlider.addEventListener("input", changeVolume);
 
-setTrackDurations();
-activateButtons();
-changeVolume();
-updateCurrentTrackDisplay();
-
-function setTrackDurations() {
-  for(var i = 0; i < tracks.length; i++) {
-    tracks[i].addEventListener("durationchange", setTrackDuration(i));
-  }
-}
-
-function setTrackDuration(trackID) {
-  return function() {
-    var seconds = tracks[trackID].duration;
-    var minutes = Math.floor(seconds / 60);
-    seconds = Math.floor(seconds % 60);
-    if(seconds < 10) {
-      seconds = "0" + seconds;
-    }
-    trackDurations[trackID].innerHTML = minutes + ":" + seconds;
-  }
-}
-
-function activateButtons() {
-  for(var i = 0; i < trackButtons.length; i++) {
-    trackButtons[i].addEventListener("click", setCurrentTrack(i));
-    //trackButtons[i].addEventListener("dblclick", togglePlayPause);
-  }
-}
-
-function setCurrentTrack(trackID) {
-  return function() {
-    setCurrentTrack(trackID);
-    pauseTrack();
-    currentTrackID = trackID;
-    currentTrack = tracks[trackID];
-    currentTrack.load();
+    setTrackDurations();
+    activateButtons();
+    changeVolume();
     updateCurrentTrackDisplay();
-    togglePlayPause();
-  }
-}
 
-function playTrack() {
-  console.log("Playing");
-  changeVolume();
-  currentTrack.play();
-  playButton.id = "pause-button";
-  playButton.title = "Pause";
-  artistImage.style.backgroundImage = "url(\"img/" + currentTrack.dataset.artistImage + "\")";
-}
+    function setTrackDurations() {
+      for(var i = 0; i < tracks.length; i++) {
+        tracks[i].addEventListener("durationchange", setTrackDuration(i));
+      }
+    }
 
-function pauseTrack() {
-  console.log("Pausing");
-  currentTrack.pause();
-  playButton.id = "play-button";
-  playButton.title = "Play";
-}
+    function setTrackDuration(trackID) {
+      return function() {
+        var seconds = tracks[trackID].duration;
+        var minutes = Math.floor(seconds / 60);
+        seconds = Math.floor(seconds % 60);
+        if(seconds < 10) {
+          seconds = "0" + seconds;
+        }
+        trackDurations[trackID].innerHTML = minutes + ":" + seconds;
+      }
+    }
 
-function togglePlayPause() {
-  if(currentTrack.paused) {
-    playTrack();
-  }
-  else {
-    pauseTrack();
-  }
-}
+    function activateButtons() {
+      for(var i = 0; i < trackButtons.length; i++) {
+        trackButtons[i].addEventListener("click", setCurrentTrack(i));
+        //trackButtons[i].addEventListener("dblclick", togglePlayPause);
+      }
+    }
 
-function changeVolume() {
-  currentTrack.volume = volumeSlider.value / 100;
-  updateVolumeText();
-  console.log("Current volume: " + currentTrack.volume);
-}
+    function setCurrentTrack(trackID) {
+      return function() {
+        setCurrentTrack(trackID);
+        pauseTrack();
+        currentTrackID = trackID;
+        currentTrack = tracks[trackID];
+        currentTrack.load();
+        updateCurrentTrackDisplay();
+        togglePlayPause();
+      }
+    }
 
-function updateVolumeText() {
-  currentVolume.innerHTML = (Math.round(currentTrack.volume * 100)) + "%";
-}
+    function playTrack() {
+      console.log("Playing");
+      changeVolume();
+      currentTrack.play();
+      playButton.id = "pause-button";
+      playButton.title = "Pause";
+      artistImage.style.backgroundImage = "url(\"img/" + currentTrack.dataset.artistImage + "\")";
+    }
 
-function updateCurrentTrackDisplay() {
-  currentTrackDisplay.innerHTML = trackTitles[currentTrackID].textContent;
-}
+    function pauseTrack() {
+      console.log("Pausing");
+      currentTrack.pause();
+      playButton.id = "play-button";
+      playButton.title = "Play";
+    }
+
+    function togglePlayPause() {
+      if(currentTrack.paused) {
+        playTrack();
+      }
+      else {
+        pauseTrack();
+      }
+    }
+
+    function changeVolume() {
+      currentTrack.volume = volumeSlider.value / 100;
+      updateVolumeText();
+      console.log("Current volume: " + currentTrack.volume);
+    }
+
+    function updateVolumeText() {
+      currentVolume.innerHTML = (Math.round(currentTrack.volume * 100)) + "%";
+    }
+
+    function updateCurrentTrackDisplay() {
+      currentTrackDisplay.innerHTML = trackTitles[currentTrackID].textContent;
+    }
 
 })();
